0ffa8498fdf9320010ea74e89f9bf20111e5b537
Hedgeken95 <ken10951128@icloud.com>
2020-09-30 14:30:37 +0900

    商品編集機能実装2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all
@@ -20,26 +21,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.valid?
-       @item.save
        redirect_to root_path
     else
-       render :new
+       render :edit
     end
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_text, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
