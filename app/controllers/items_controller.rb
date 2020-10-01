16465be093e582f37d87e5c4533cb6573c503cbd
Hedgeken95 <ken10951128@icloud.com>
2020-10-01 11:38:13 +0900

    商品編集機能実装6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :login, only: [:update]
+  before_action :login, only: [:update, :edit]
 
   def index
-    @items = Item.all
     @items = Item.order("created_at DESC")
   end
 
