261f11f4ddd7233746dea17230e275f7e136650e
Hedgeken95 <ken10951128@icloud.com>
2020-10-07 17:57:35 +0900

    gemの追加

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 gem 'payjp'
 gem "aws-sdk-s3", require: false
+gem 'ransack'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'items#index'
   resources :items do
     resources :purchases, only:[:index, :create]
   end
