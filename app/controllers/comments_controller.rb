5cee111991fde3044ea69195bfa7f67633f44879
Hedgeken95 <ken10951128@icloud.com>
2020-10-09 17:39:53 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,5 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
